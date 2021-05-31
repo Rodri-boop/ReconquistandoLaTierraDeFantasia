95407842809734347b6959d366349b27f1a5f507
Brenda (๑و•̀ω•́)و <85079729+BrendaRocha@users.noreply.github.com>
2021-05-30 22:42:55 -0300

    implementamos batallar, descansar,aniadirAliado

--- src/Razas/Ejercito.java
+++ src/Razas/Ejercito.java
@@ -1,162 +1,138 @@
 package Razas;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.Stack;
-
 import Razas.Poblado.Bando;
 import Razas.Raza.Status;
 
+
 public class Ejercito extends Unidad {
 	
-	//hola esto es de prueba
+	
 
-	private ArrayList [] army;
-	private ArrayList <Unidad> aliados= new ArrayList<Unidad>();
-	private ArrayList <Unidad> propios= new ArrayList<Unidad>();
-	private ArrayList <Unidad> propiosHeridos= new ArrayList<Unidad>();
-	private ArrayList <Unidad> aliadosHeridos= new ArrayList<Unidad>();
+	private PriorityQueue<Raza> army = new PriorityQueue<Raza>();
+	private PriorityQueue<Raza> auxiliar = new PriorityQueue<Raza>();
+
+	/**
+	 * 
+	 * @param raza
+	 * @param cantidad
+	 */
 	public Ejercito(String raza ,int cantidad) {
-		army= new ArrayList[4];
-		army[0]= aliados;
-		army[1]= propios;
-		army[2]= propiosHeridos;
-		army[3]= aliadosHeridos;
+		
 		aniadirPropios(raza,cantidad);
 		
 	}
+	/**
+	 * 
+	 * @param raza
+	 * @param cantidad
+	 */
 	private void aniadirPropios(String raza ,int cantidad) {
 		Raza soldado;
 		if(raza== "Nortaichian") {
 			
-			for(int i =0;i<cantidad;i++) {
+			for(int i =0;i<cantidad;i++) {	
 			   soldado= new Nortaichian();
-				propios.add(soldado);
+				army.add(soldado);
 			}
 			
 		}if(raza== "Radaiteran") {
 			
 			for(int i =0;i<cantidad;i++) {
 				soldado= new Radaiteran();
-				propios.add(soldado);
+				army.add(soldado);
 			}
 			
 		}if(raza== "Reralopes") {
 			
 			for(int i =0;i<cantidad;i++) {
 				soldado= new Reralopes();
-				propios.add(soldado);
+				army.add(soldado);
 			}
 		}if(raza== "Wrives") {
 		
 			for(int i =0;i<cantidad;i++) {
 				soldado= new Wrives();
-				propios.add(soldado);
+				army.add(soldado);
 			}
 			
 		}
 	}
-	private void aniadirAliados(String raza ,int cantidad) {
-		Raza soldado;
-		if(raza== "Nortaichian") {
-			
-			for(int i =0;i<cantidad;i++) {
-			   soldado= new Nortaichian();
-				aliados.add(soldado);
-			}
-			
-		}if(raza== "Radaiteran") {
-			
-			for(int i =0;i<cantidad;i++) {
-				soldado= new Radaiteran();
-				aliados.add(soldado);
-			}
-			
-		}if(raza== "Reralopes") {
-			
-			for(int i =0;i<cantidad;i++) {
-				soldado= new Reralopes();
-				aliados.add(soldado);
-			}
-		}if(raza== "Wrives") {
-		
-			for(int i =0;i<cantidad;i++) {
-				soldado= new Wrives();
-				aliados.add(soldado);
-			}
-			
-		}
-	}
+
+	/**
+	 * 
+	 * @param pueblito
+	 */
 	public void comprobarPoblado(Poblado pueblito) {
-		//TODO: comprobar si al llegar a un pueblo los ejercitos son aliados o enemigos
+	
 		if(pueblito.getBando()== Bando.ENEMIGO) {
 			batallar(pueblito);
+		}else {
+			descansar();
+			aniadirAliados(pueblito.getPoblado());
 		}
 	}
 	
 	
 	
-
+/**
+ * 
+ * @param poblado
+ */
+	private void aniadirAliados(Queue<Raza> poblado) {
+		while(!poblado.isEmpty()) {
+			army.add(poblado.poll());
+		}
+		
+	}
+	/**
+	 * 
+	 * @param pueblito
+	 */
 	private void batallar(Poblado pueblito) {
 		Queue <Raza> enemigos= pueblito.getPoblado();
-		Iterator it = aliados.iterator();
-		while(!enemigos.isEmpty() && it.hasNext()) {
-			Raza enemigoActual= enemigos.poll();
-			Raza aliadoActual =  (Raza) it.next();
-			it.remove();
+		Raza enemigoActual= null;
+		Raza aliadoActual= null;
+		while(!enemigos.isEmpty() && !army.isEmpty()) {
+		    enemigoActual= enemigos.poll();
+			aliadoActual =  army.poll();
 			
-			//pelea individual fighting
+			//pelea individual
 			while(enemigoActual.getStatus()!= Status.DESMAYADO && aliadoActual.getStatus()!= Status.DESMAYADO){
 				aliadoActual.atacar(enemigoActual);
 				if(enemigoActual.getStatus()!= Status.DESMAYADO)
 					enemigoActual.atacar(aliadoActual);
 				
 			}
-		}
-	}
-	@Override
-	protected void descansar() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	
-	/*
-	 * private Stack<Unidad> army;
-	 * public Ejercito(int cantidad,String racita) {
-		army = new Stack<Unidad>();
-		agregarAliados(cantidad,racita);
-	}
-	
-	
-	private void agregarAliados(int cantidad, String racita) {
-		if(racita=="Wrives") {
-			for(int i = 0; i < cantidad; i++) {
-				Unidad unidad = new Wrives();
-				army.add(unidad);
+			if(enemigoActual.getStatus()== Status.DESMAYADO&& !enemigos.isEmpty()) {
+				enemigoActual = enemigos.poll();
+			}
+			if(aliadoActual.getStatus()== Status.DESMAYADO&& !army.isEmpty()) {
+				aliadoActual = army.poll();
 			}
 		}
-		
+		if(enemigoActual.getStatus()== Status.DESMAYADO) {
+			army.add(aliadoActual);
+		}else {
+			enemigos.add(enemigoActual);
+		}
 	}
-	
-	
-	
-	@Override
-	protected void atacar(Raza enemigos) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	/**
+	 * 
+	 */
 	@Override
 	protected void descansar() {
-		//Si es aliado descansa y suma tropas
-		
-		// TODO Auto-generated method stub
+		while(!army.isEmpty()) {
+			auxiliar.add(army.poll());
+		}
+		while(!auxiliar.isEmpty()) {
+			Raza soldado= auxiliar.poll();
+			soldado.descansar();
+			army.add(soldado);
+		}
 		
 	}
 
-	 */
-	
 	
 }
